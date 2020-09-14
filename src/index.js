1bba1dac520501ccfd30cf73ec205720edbfa490
Maria <marybelousova@yahoo.com>
2020-09-14 18:41:46 -0400

    db connect

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import logger from './modules/core/logger'
 import parseResponse from './modules/core/parseResponse'
 import routes from './modules/core/routes';
 import cors from './modules/core/cors';
+import dbConnect from './modules/core/db';
 
 const app = express();
 const PORT = 5000;
 
+dbConnect();
 logger(app);
 parseResponse(app);
 cors(app);
-routes(app)
+routes(app);
 
 // app.get('/', home);
 // app.post('/info', info);
